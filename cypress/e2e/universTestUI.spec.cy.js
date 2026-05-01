74c8a995b0d981472db93ac713ad0591d76bb3e9
tiffanychen <tiffanyc1213@gmail.com>
2026-05-01 19:16:06 +0800

    cleanup unused folders

--- cypress/e2e/universTestUI.spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('./index.html')
-    cy.get('#navbar-collapse > ul > li:nth-child(2) > a').click()
-    cy.get('#navbar-collapse > ul > li:nth-child(3) > a').click()
-    cy.get('#navbar-collapse > ul > li:nth-child(4) > a').click()
-    cy.get('#navbar-collapse > ul > li:nth-child(1) > a').click()
-  })
-})
